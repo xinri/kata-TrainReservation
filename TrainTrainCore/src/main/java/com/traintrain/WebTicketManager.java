00cc485e567c67c656666011bcfe2062ec133085
Bruno Boucard <boucard.bruno@free.fr>
2018-10-16 09:02:12 +0200

    close client (REST)

--- TrainTrainCore/src/main/java/com/traintrain/WebTicketManager.java
+++ TrainTrainCore/src/main/java/com/traintrain/WebTicketManager.java
@@ -56,8 +56,12 @@ public class WebTicketManager {
                         train);
             } else {
                 Client client = ClientBuilder.newClient();
-                bookingRef = getBookRef(client);
-
+                try {
+                    bookingRef = getBookRef(client);
+                }
+                finally {
+                    client.close();
+                }
                 for (Seat availableSeat : availableSeats) {
                     availableSeat.setBookingRef(bookingRef);
                     numberOfReserv++;
@@ -79,10 +83,14 @@ public class WebTicketManager {
                 String postContent = buildPostContent(train, bookingRef, availableSeats);
 
                 Client client = ClientBuilder.newClient();
-                WebTarget webTarget = client.target(urITrainDataService + "/reserve/");
-                Invocation.Builder request = webTarget.request(MediaType.APPLICATION_JSON_TYPE);
-                request.post(Entity.text(postContent));
-
+                try {
+                    WebTarget webTarget = client.target(urITrainDataService + "/reserve/");
+                    Invocation.Builder request = webTarget.request(MediaType.APPLICATION_JSON_TYPE);
+                    request.post(Entity.text(postContent));
+                }
+                finally {
+                    client.close();
+                }
                 return String.format(
                         "{{\"train_id\": \"%s\", \"booking_reference\": \"%s\", \"seats\": %s}}",
                         train,
@@ -140,14 +148,17 @@ public class WebTicketManager {
 
     protected String getTrain(String train) {
         String JsonTrainTopology;
-
         Client client = ClientBuilder.newClient();
+        try {
 
-        WebTarget target = client.target(urITrainDataService + "/api/data_for_train/");
-        WebTarget path = target.path(String.valueOf(train));
-        Invocation.Builder request = path.request(MediaType.APPLICATION_JSON);
-        JsonTrainTopology = request.get(String.class);
-
+            WebTarget target = client.target(urITrainDataService + "/api/data_for_train/");
+            WebTarget path = target.path(String.valueOf(train));
+            Invocation.Builder request = path.request(MediaType.APPLICATION_JSON);
+            JsonTrainTopology = request.get(String.class);
+        }
+        finally {
+            client.close();
+        }
         return JsonTrainTopology;
     }
 
